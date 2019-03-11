24212717ebab44b513e11300c23cb08b50288b32
Adeniyi <adesanoye.samson@yahoo.com>
2019-03-11 04:39:25 -0700

    output os details to screen

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## README
 # check os compactibily with software
 ##version 1 run with node
+
+#v.2 run own browser by creating command and server
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const os = require('os');
 
-//platform
-console.log(os.platform());
-//cpu arch
-console.log(os.arch());
-//cpu core info
-console.log(os.cpus());
-//free memory
-console.log(os.freemem());
+// //platform
+// console.log(os.platform());
+// //cpu arch
+// console.log(os.arch());
+// //cpu core info
+// console.log(os.cpus());
+// //free memory
+// console.log(os.freemem());
 
 //store in variables
 const uPlatform = os.platform();
-const uarch = os.arch();
-const ucpu = os.cpus();
-const ufreemem = os.freemem();
+const uArch = os.arch();
+const uCpu = os.cpus();
+const user = os.userInfo();
+const uFreemem = Math.ceil(os.freemem() / 43);
+
+
+console.log(`Welcome ${user.username}`);
+console.log(`\t Platform: ${uPlatform} \n \t Architecture: ${uArch} `);
+console.log(`\t FreeMemory: ${uFreemem} \n \t Architecture: ${uArch} `);
