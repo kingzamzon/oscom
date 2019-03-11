20b6d2198b8369cd4b4f257dbd7b51979b7e2edd
Adeniyi <adesanoye.samson@yahoo.com>
2019-03-11 04:11:03 -0700

    got user os details

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-samson
+const os = require('os');
+
+//platform
+console.log(os.platform());
+//cpu arch
+console.log(os.arch());
+//cpu core info
+console.log(os.cpus());
+//free memory
+console.log(os.freemem());
