bfa7faa71db8024f02fd6cb42a612fe4be89c46f
Adeniyi <adesanoye.samson@yahoo.com>
2019-03-12 14:06:27 -0700

    comment ifelse and loop through softWares array

--- index.js
+++ index.js
@@ -8,16 +8,26 @@ const uArch = os.arch();
 const uCpu = os.cpus();
 const user = os.userInfo();
 const uFreemem = Math.ceil(os.freemem() / 43);
-
+const softWares = [
+    { name: 'GTA5', spec: 54323 },
+    { name: 'GTA4' }
+];
 
 console.log(`Welcome ${user.username}`);
 var softname = process.argv[2];
 
-if (softname === 'GTA5'){
-    console.log('software name: ', softname);
-}else {
-    console.log("Software not recongnized")
-}
+softWares.forEach(name => {
+    if (name.name === softname) {
+        console.log(name.name);
+    }
+
+});
+
+// if (softname === 'GTA5') {
+//     console.log('software name: ', softname);
+// } else {
+//     console.log("Software not recongnized")
+// }
 console.log("Your OS detials");
 console.log(`\t Platform: ${uPlatform} \n \t Architecture: ${uArch} `);
 console.log(`\t FreeMemory: ${uFreemem} \n \t Architecture: ${uArch} `);
